28cd5dde05b2767d191384764080e666f4ba2ce0
Rocksteady81 <honza.davidek@gmail.com>
2020-05-10 23:31:47 +0200

    JMX added

---
+++ elastic-apm-jmx/src/main/java/cz/tipsport/elastic/apm/jmx/ExamplesApplication.java
@@ -0,0 +1,30 @@
+package cz.tipsport.elastic.apm.jmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class ExamplesApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = SpringApplication.run(cz.tipsport.elastic.apm.jmx.ExamplesApplication.class, args);
+
+		cz.tipsport.elastic.apm.jmx.TestMbean obj = (cz.tipsport.elastic.apm.jmx.TestMbean) applicationContext.getBean("testMbean");
+		obj.show();
+		try{
+		//Sleep for 2 min 
+		//Open JConsole and select your process Bean click PD > testBean > Operation > resetMessageViaMBean
+		//Click on 	resetMessageViaMBean - if you see testMbean - it is resetting message
+	    //Check whats the message after sleep time is over
+		Thread.sleep(120000); 
+		}catch(Exception e){
+			
+		}
+		
+		obj.show();
+		
+	}
+
+}
+
---
+++ elastic-apm-jmx/src/main/java/cz/tipsport/elastic/apm/jmx/TestMbean.java
@@ -0,0 +1,35 @@
+package cz.tipsport.elastic.apm.jmx;
+
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Component;
+
+@ManagedResource(
+        objectName="PD:category=MBeans,name=testBean",
+        description="Managed Bean",
+        log=true, logFile="jmx-platform.log")
+@Component("testMbean")
+public class TestMbean {
+	
+	private String message = "Simple Message";
+	
+	public TestMbean(){
+		System.out.println("......TestMbean........");
+	}
+	
+	@ManagedOperation
+	public void resetMessageViaMBean(){
+		this.message = "Message RESET";
+	}
+
+	@ManagedAttribute(description="Message name")
+	public String getMessageName() {
+		return message;
+	}
+
+	public void show(){
+		System.out.println(message);
+	}
+
+}
